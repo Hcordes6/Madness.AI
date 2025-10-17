e7e529b28188b56a2deebb073015cf0e2ee607b1
Hcordes6 <hcordesmn@gmail.com>
2025-10-17 17:21:35 -0500

    Updated proxy to be self hosted. Tested and ensured the page fetching idea

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const url = 'https://ncaa-api.henrygd.me/stats/basketball-men/d1/current/team/474/p2';
-const proxy = 'https://corsproxy.io/?'; // or use https://api.allorigins.win/raw?url=
+const url = 'https://ncaa-api.henrygd.me/stats/basketball-men/d1/current/team/474/';
+const appendP2 = 'p2'; 
+const proxy = 'https://corsbypassproxy.hcordesmn.workers.dev/?url='; // or use https://api.allorigins.win/raw?url=
 
 fetch(proxy + encodeURIComponent(url))
   .then(res => res.json())
   .then(data => {
       console.log(data);
+      document.body.innerHTML += `<h1>${data.pages} </h1>`;
       data.data.forEach(element => {
           const rank = element.Rank;
           document.body.innerHTML += `<h1>${rank} </h1>`;
@@ -13,4 +15,7 @@ fetch(proxy + encodeURIComponent(url))
       // Do something with teamStats
   })
   .catch(err => console.error(err));
+
+
+
 
